4020094b9e012faed15396471b469017bc4d4dca
yrgk <yarik090908@gmail.com>
2024-06-17 21:45:25 +0300

    created catalog base

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -5,9 +5,12 @@ export default function CatalogItem(props: CatalogItemProps) {
         <>
         <div className="catalogItem">
             {/* <img className="catalogItemCover" src={props.coverUrl} alt="Not loaded" loading="lazy" /> */}
-            {/* <div className="catalogItemCoverBox"></div> */}
-            <h2>{props.price}{props.currency}</h2>
-            <h2>{props.title}</h2>
+            <div className="catalogItemCoverBox"></div>
+            <div className="catalogItemTextBox">
+                <h3 id="price">{ props.price }{ props.currency }</h3>
+                <h3>{ props.title.slice(0, 1) }...</h3>
+            </div>
+
         </div>
         </>
     )
