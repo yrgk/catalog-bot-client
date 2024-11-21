c26a5013f2fa9a47dc960dd49ac125ec4b4e2d1b
yrgk <yarik090908@gmail.com>
2024-11-21 22:14:17 +0300

    test

--- src/components/CatalogList/CatalogList.tsx
+++ src/components/CatalogList/CatalogList.tsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import CatalogHeader from '../CatalogHeader/CatalogHeader'
-import CatalogItem from '../CatalogItem/CatalogItem'
+// import CatalogItem from '../CatalogItem/CatalogItem'
 import './CatalogList.css'
 
 const apiClient = axios.create({
@@ -15,10 +15,10 @@ const apiClient = axios.create({
 });
 
 export default function CatalogList() {
-    const [itemCount, setItemCount] = useState(0);
-    const [totalCost, setTotalCost] = useState(0);
-    const [items, setItems] = useState<CatalogItem[]>([]);
-    const [currency, setCurrency] = useState("")
+    // const [itemCount, setItemCount] = useState(0);
+    // const [totalCost, setTotalCost] = useState(0);
+    // const [items, setItems] = useState<CatalogItem[]>([]);
+    // const [currency, setCurrency] = useState("")
     const [shopTitle, setShopTitle] = useState("")
     let { shopId } = useParams();
     const tg = WebApp;
@@ -41,37 +41,37 @@ export default function CatalogList() {
     //         })
     // }
 
-    const fetchCatalog = (shop_id: number) => {
-        apiClient.get(`api/v1/catalog/${shop_id}`)
-            .then((response) => {
-                setItems(response.data.items);
-                setShopTitle(response.data.shop_title)
-                setCurrency(response.data.currency)
-            })
-    }
+    // const fetchCatalog = (shop_id: number) => {
+    //     apiClient.get(`api/v1/catalog/${shop_id}`)
+    //         .then((response) => {
+    //             setItems(response.data.items);
+    //             setShopTitle(response.data.shop_title)
+    //             setCurrency(response.data.currency)
+    //         })
+    // }
 
-    useEffect(() => {
-        // fetchCatalog()
-        fetchCatalog(Number(shopId))
-        tg.onEvent('mainButtonClicked', function() {
-            tg.HapticFeedback.impactOccurred('heavy')
-        })
-    }, []);
+    // useEffect(() => {
+    //     // fetchCatalog()
+    //     fetchCatalog(Number(shopId))
+    //     tg.onEvent('mainButtonClicked', function() {
+    //         tg.HapticFeedback.impactOccurred('heavy')
+    //     })
+    // }, []);
 
-    const onAdd = (price: number) => {
-        setItemCount(prevItemCount => {
-            const newCount = prevItemCount + 1;
-            return newCount;
-        });
-        setTotalCost(prevTotalCost => prevTotalCost + price);
+    // const onAdd = (price: number) => {
+    //     setItemCount(prevItemCount => {
+    //         const newCount = prevItemCount + 1;
+    //         return newCount;
+    //     });
+    //     setTotalCost(prevTotalCost => prevTotalCost + price);
 
-        // Показать главную кнопку после первого добавления товара
-        tg.MainButton.show();
-        tg.HapticFeedback.impactOccurred('medium');
-        tg.MainButton.setParams({
-            text: `${itemCount + 1} товаров | ${totalCost + price}RUB`
-        });
-    };
+    //     // Показать главную кнопку после первого добавления товара
+    //     tg.MainButton.show();
+    //     tg.HapticFeedback.impactOccurred('medium');
+    //     tg.MainButton.setParams({
+    //         text: `${itemCount + 1} товаров | ${totalCost + price}RUB`
+    //     });
+    // };
 
     return (
         <>
@@ -79,7 +79,8 @@ export default function CatalogList() {
         {/* <h1>{`shop: ${shopId}`}</h1> */}
         {/* <h1>{`count: ${itemCount}, cost: ${totalCost}`}</h1> */}
         <div className='catalogList'>
-            {items.map((item) => (
+            <h1>{shopId}</h1>
+            {/* {items.map((item) => (
                 <CatalogItem
                     id={item.id}
                     title={item.title}
@@ -90,7 +91,7 @@ export default function CatalogList() {
                     shop_id={Number(shopId)}
                     onAdd={onAdd}
                 />
-            ))}
+            ))} */}
         </div>
         </>
     )
