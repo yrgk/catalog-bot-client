5f33d613edfaa179dc58c61d7c330522531f2281
yrgk <yarik090908@gmail.com>
2024-11-21 22:24:06 +0300

    test fix

--- src/components/CatalogList/CatalogList.tsx
+++ src/components/CatalogList/CatalogList.tsx
@@ -1,25 +1,25 @@
 import WebApp from '@twa-dev/sdk'
-// import axios from 'axios'
-// import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 // import { useParams } from 'react-router-dom'
-// import CatalogHeader from '../CatalogHeader/CatalogHeader'
-// import CatalogItem from '../CatalogItem/CatalogItem'
+import CatalogHeader from '../CatalogHeader/CatalogHeader'
+import CatalogItem from '../CatalogItem/CatalogItem'
 import './CatalogList.css'
 
-// const apiClient = axios.create({
-//     baseURL: "http://185.197.75.220:8000/",
-//     headers: {
-//         'Access-Control-Allow-Origin': '*',
-//         'Content-Type': 'application/json',
-//     },
-// });
+const apiClient = axios.create({
+    baseURL: "http://185.197.75.220:8000/",
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+    },
+});
 
 export default function CatalogList() {
-    // const [itemCount, setItemCount] = useState(0);
-    // const [totalCost, setTotalCost] = useState(0);
-    // const [items, setItems] = useState<CatalogItem[]>([]);
-    // const [currency, setCurrency] = useState("")
-    // const [shopTitle, setShopTitle] = useState("")
+    const [itemCount, setItemCount] = useState(0);
+    const [totalCost, setTotalCost] = useState(0);
+    const [items, setItems] = useState<CatalogItem[]>([]);
+    const [currency, setCurrency] = useState("")
+    const [shopTitle, setShopTitle] = useState("")
     // let { shopId } = useParams();
     const tg = WebApp;
     let shopId = tg.initDataUnsafe.start_param
@@ -41,46 +41,45 @@ export default function CatalogList() {
     //         })
     // }
 
-    // const fetchCatalog = (shop_id: number) => {
-    //     apiClient.get(`api/v1/catalog/${shop_id}`)
-    //         .then((response) => {
-    //             setItems(response.data.items);
-    //             setShopTitle(response.data.shop_title)
-    //             setCurrency(response.data.currency)
-    //         })
-    // }
+    const fetchCatalog = (shop_id: number) => {
+        apiClient.get(`api/v1/catalog/${shop_id}`)
+            .then((response) => {
+                setItems(response.data.items);
+                setShopTitle(response.data.shop_title)
+                setCurrency(response.data.currency)
+            })
+    }
 
-    // useEffect(() => {
-    //     // fetchCatalog()
-    //     fetchCatalog(Number(shopId))
-    //     tg.onEvent('mainButtonClicked', function() {
-    //         tg.HapticFeedback.impactOccurred('heavy')
-    //     })
-    // }, []);
+    useEffect(() => {
+        // fetchCatalog()
+        fetchCatalog(Number(shopId))
+        tg.onEvent('mainButtonClicked', function() {
+            tg.HapticFeedback.impactOccurred('heavy')
+        })
+    }, []);
 
-    // const onAdd = (price: number) => {
-    //     setItemCount(prevItemCount => {
-    //         const newCount = prevItemCount + 1;
-    //         return newCount;
-    //     });
-    //     setTotalCost(prevTotalCost => prevTotalCost + price);
+    const onAdd = (price: number) => {
+        setItemCount(prevItemCount => {
+            const newCount = prevItemCount + 1;
+            return newCount;
+        });
+        setTotalCost(prevTotalCost => prevTotalCost + price);
 
-    //     // Показать главную кнопку после первого добавления товара
-    //     tg.MainButton.show();
-    //     tg.HapticFeedback.impactOccurred('medium');
-    //     tg.MainButton.setParams({
-    //         text: `${itemCount + 1} товаров | ${totalCost + price}RUB`
-    //     });
-    // };
+        // Показать главную кнопку после первого добавления товара
+        tg.MainButton.show();
+        tg.HapticFeedback.impactOccurred('medium');
+        tg.MainButton.setParams({
+            text: `${itemCount + 1} товаров | ${totalCost + price}RUB`
+        });
+    };
 
     return (
         <>
-        {/* <CatalogHeader title={shopTitle}/> */}
-        <h1>{`shop: ${shopId}`}</h1>
+        <CatalogHeader title={shopTitle}/>
         {/* <h1>{`count: ${itemCount}, cost: ${totalCost}`}</h1> */}
         <div className='catalogList'>
             <h1>{shopId}</h1>
-            {/* {items.map((item) => (
+            {items.map((item) => (
                 <CatalogItem
                     id={item.id}
                     title={item.title}
@@ -91,7 +90,7 @@ export default function CatalogList() {
                     shop_id={Number(shopId)}
                     onAdd={onAdd}
                 />
-            ))} */}
+            ))}
         </div>
         </>
     )
